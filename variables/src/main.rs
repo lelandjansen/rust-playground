9ff0568658f04febe815c4d6e2f59c1cc9e26d76
Leland Jansen <lelandjansen@users.noreply.github.com>
2018-04-14 10:56:14 -0600

    Rust book variables chapter

---
+++ variables/src/main.rs
@@ -0,0 +1,13 @@
+const Y: i32 = 900_000;
+
+fn main() {
+    println!("Y = {}", Y);
+    let mut x = 5;
+    println!("x = {}", x);
+    x = 6;
+    println!("x = {}", x);
+    let z = 7;
+    let z = z * 2;
+    let z = z - 1;
+    println!("z = {}", z);
+}
