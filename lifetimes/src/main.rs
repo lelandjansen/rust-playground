55fcb3095af42fe91b6edf61032f52ebeb6a36c8
Leland Jansen <lelandjansen@users.noreply.github.com>
2018-04-30 15:21:37 -0700

    Lifetimes

---
+++ lifetimes/src/main.rs
@@ -0,0 +1,38 @@
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let string1 = String::from("abcd");
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is \"{}\"", result);
+    }
+
+    println!("The longest string is \"{}\"", longest("abc", "wxyz"));
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first_sentence };
+    println!("Important excerpt: \"{}\"", i.part);
+
+    let _ = i.announce_and_return_part("oahi");
+}
