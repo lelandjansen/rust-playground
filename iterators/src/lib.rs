b0c94e8776bbc3a1e2b685a5a3ba838c6ca33c72
Leland Jansen <lelandjansen@users.noreply.github.com>
2018-07-31 23:17:36 -0700

    Processing a series of items with iterators

---
+++ iterators/src/lib.rs
@@ -0,0 +1,93 @@
+#[derive(PartialEq, Debug)]
+pub struct Shoe {
+    size: u32,
+    style: String,
+}
+
+pub fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes
+        .into_iter()
+        .filter(|shoe| shoe.size == shoe_size)
+        .collect()
+}
+
+#[test]
+fn filters_by_size() {
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 13,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 10,
+            style: String::from("boot"),
+        },
+        Shoe {
+            size: 12,
+            style: String::from("slipper"),
+        },
+    ];
+    let in_my_size = shoes_in_my_size(shoes, 10);
+    assert_eq!(
+        in_my_size,
+        vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ]
+    );
+}
+
+pub struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    pub fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+    for expected in 1..10 {
+        if expected < 6 {
+            assert_eq!(counter.next(), Some(expected));
+        } else {
+            assert_eq!(counter.next(), None);
+        }
+    }
+}
+
+#[test]
+fn usering_other_iterator_trait_methods() {
+    let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    assert_eq!(18, sum);
+}
---
+++ iterators/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let v1 = vec![1, 2, 3];
+
+    for val in v1.iter() {
+        println!("Got: {}", val);
+    }
+
+    let mut v1_iter = v1.iter();
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+
+    let v1_iter = v1.iter();
+    let total: i32 = v1_iter.sum();
+    assert_eq!(total, 6);
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+    let v1: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    assert_eq!(v1, vec![2, 3, 4]);
+}
