6e0781b787ae80ed86f4d1f91badb10f962e172e
Leland Jansen <lelandjansen@users.noreply.github.com>
2018-09-23 14:39:13 -0700

    Advanced: Types

---
+++ advanced/types/src/main.rs
@@ -0,0 +1,10 @@
+type Kilometers = i32;
+
+fn main() {
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+    println!("x + y = {}", x + y);
+
+    let s1: &str = "Hello there!";
+    let s2: &str = "How's it going?";
+}
